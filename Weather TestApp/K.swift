89f0f9459bdb5b774cbce2390b0558c9417a284d
Aydin Aghayev <agayev.aydin@gmail.com>
2022-06-09 13:50:25 +0400

    Create K.swift

---
+++ Weather TestApp/K.swift
@@ -0,0 +1,18 @@
+//
+//  K.swift
+//  Weather TestApp
+//
+//  Created by Aydin Aghayev on 6/9/22.
+//
+
+import Foundation
+
+struct K {
+    
+    static let apiKey = "4fbfa6660ea4f06f7cbee5873c70e917"
+    static let url = "https://api.openweathermap.org/data/2.5/"
+}
+
+//api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+//api.openweathermap.org/data/2.5/weather?q=London&appid={API key}
+//40.587518116908925, 49.65305325525847
